3724705ddcca4e187879208f32bff4989951f185
vishal62189 <46949075+vishal62189@users.noreply.github.com>
2025-02-14 18:50:41 +0530

    Update lighthouse-config.js

--- lighthouse-config.js
+++ lighthouse-config.js
@@ -1,9 +1,16 @@
 module.exports = {
   ci: {
     collect: {
-      url: ['https://www.tatacliq.com/'],  // URLs to test
+      url: ['https://example.com'],  // URLs to test
       settings: {
-        formFactor: 'desktop',  // Use desktop emulation
+        formFactor: 'desktop',  // Use 'desktop' or 'mobile'
+        screenEmulation: {
+          disabled: false,  // Enable screen emulation
+          width: 1350,      // Desktop screen width
+          height: 940,      // Desktop screen height
+          deviceScaleFactor: 1,
+          mobile: false,    // Disable mobile emulation
+        },
         throttling: {
           rttMs: 40,
           throughputKbps: 10 * 1024,
